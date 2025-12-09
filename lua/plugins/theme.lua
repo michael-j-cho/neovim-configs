6e31c0fb038f8ad597df6b7b917297f08b4ce0e0
Michael Cho <mcho314@gatech.edu>
2025-12-09 10:57:28 -0500

    Commented out unnecessary theme settings for catppuccin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,15 +1,15 @@
 return {
   -- 1. Install the Catppuccin plugin
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    -- Optional: Configure Catppuccin specifics here (like transparency)
-    opts = {
-      term_colors = true,
-      transparent_background = true,
-    },
-  },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   -- Optional: Configure Catppuccin specifics here (like transparency)
+  --   opts = {
+  --     term_colors = true,
+  --     transparent_background = true,
+  --   },
+  -- },
 
   -- 2. Configure LazyVim to load 'catppuccin-frappe' by default
   {
